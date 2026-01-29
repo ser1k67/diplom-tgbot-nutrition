6e496d76f606e3af3775ced674c5f3f5b852bfe5
j724 <j7248855@gmail.com>
2026-01-29 17:10:05 +0500

    Готовая регистрация

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -2,22 +2,19 @@ package main
 
 import (
 	"diplomkabot/internal/db"
-	"fmt"
+	"diplomkabot/internal/route"
 
 	"os"
 
-	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	tg "gopkg.in/telebot.v3"
 )
 
 func main() {
-	//Инициализация сервера
-	server := gin.Default()
 	//Открытие файла .env для чтения
 	err := godotenv.Load("../../configs/config.env")
 	if err != nil {
-		fmt.Println("Не найден .env файл, пожалуйста загрузите его в папку configs в корне проекта:", err)
+		println("Не найден .env файл, пожалуйста загрузите его в папку configs в корне проекта:", err)
 		return
 	}
 	//Открываем подключени к базе данных
@@ -29,9 +26,9 @@ func main() {
 		Token: os.Getenv("TELEGRAM_BOT_TOKEN"),
 	})
 	if err != nil {
-		fmt.Println("Не получилось подключиться к боту", err)
+		println("Не получилось подключиться к боту", err)
 	}
-	println("Сервер запущен на порте: 8282")
-	server.Run("8282") //запуск сервера
-
+	route.Routers(bot)
+	println("Бот запущен")
+	bot.Start()
 }
--- internal/handler/registration.go
+++ internal/handler/registration.go
@@ -1,6 +1,8 @@
 package handler
 
 import (
+	"fmt"
+
 	tb "gopkg.in/telebot.v3"
 )
 
@@ -21,6 +23,10 @@ type AllInformation struct {
 func Registration(bot *tb.Bot, c tb.Context) error {
 	var err error
 	//Выбор языка
+	err = c.Send("Выберите язык / Тілді таңдаңыз")
+	if err != nil {
+		return fmt.Errorf("Не удалось отправить сообщение пользователю")
+	}
 
 	return err
 }
